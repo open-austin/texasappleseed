6547830b5c0f93b2689e1bd405af028a2cfbca81
wilsaj <wilson.andrew.j+github@gmail.com>
2016-01-16 17:26:51 -0600

    fix: make href urls cleaner and make selected/hover style stick

--- discipline-map.js
+++ discipline-map.js
@@ -7,36 +7,42 @@ window.onload = function() {
   cartodb.createVis(map, visUrl)
     .on('done', function(vis, layers) {
       $.each(layers[1].layers, function(idx, layer) {
-        toggleLayers[layer.layer_name] = layer;
+        var layerId = normalizeForCSS(layer.layer_name);
+        toggleLayers[layerId] = layer;
 
         var element = $('<a>')
-          .attr('id', layer.layer_name)
-          .attr('href', '#' + layer.layer_name)
+          .attr('id', layerId)
+          .attr('href', '#' + layerId)
           .addClass('button')
-          .addClass(layer.layer_name)
+          .addClass(layerId)
           .text(layer.layer_name)
           .click(function (event) {
             var clickedName = event.target.id;
             setLayer(clickedName);
           });
+
         $('#menu').prepend(element);
 
-        if (linkedName !== '' && toggleLayers.hasOwnProperty(linkedName)) {
+        if (linkedName && toggleLayers.hasOwnProperty(linkedName)) {
           setLayer(linkedName);
         } else if (idx === 0) {
-          setLayer(layer.layer_name);
+          setLayer(layerId);
         }
       });
     });
 
-  function setLayer(layerName) {
+  function setLayer(layerId) {
     $.each(toggleLayers, function(name, layer) {
       layer.sub.hide();
     });
 
-    toggleLayers[layerName].sub.show();
+    toggleLayers[layerId].sub.show();
 
     $('.button').removeClass('selected');
-    $('#' + layerName).addClass('selected');
+    $('#' + layerId).addClass('selected');
+  }
+
+  function normalizeForCSS(str) {
+    return str.replace(/[^a-z0-9\ \-]/gmi, '').replace(/\ /g, '-')
   }
 }
